dee3045170574fc568ba881118e2604a42a33e17
lazona24hs <jonacaceresok@gmail.com>
2026-09-05 08:28:14 -0300

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,8 +2,7 @@
 
 LOGO_URL="https://pub-f00d5d649500451fb2fe8979f4685eea.r2.dev/logo.png"
 
-# URL de tu publicidad alojada en GitHub
-ADS_URL="https://lazona24hs.github.io/ziptv/eradiobanner.png"
+
 
 # Lista de tus videos .mp4 en R2
 VIDEOS=(
@@ -25,9 +24,8 @@ VIDEOS=(
   "https://pub-f00d5d649500451fb2fe8979f4685eea.r2.dev/KAROL%20G%2C%20Judeline%2C%20rusowsky%20-%20BbY%20WOW%20(Visualizer).mp4"
 )
 
-# Descargar las imágenes
+# Descargar el logo localmente
 curl -s -o logo.png "$LOGO_URL"
-curl -s -o eradiobanner.png "$ADS_URL"
 
 while true; do
   RANDOM_INDEX=$((RANDOM % ${#VIDEOS[@]}))
@@ -35,13 +33,11 @@ while true; do
 
   echo "Transmitiendo: $VIDEO_URL"
 
-  ffmpeg -re -i "$VIDEO_URL" -i logo.png -i eradiobanner.png \
+  # Transmisión únicamente con el logo al 35% del ancho de pantalla
+  ffmpeg -re -i "$VIDEO_URL" -i logo.png \
     -filter_complex \
-    "[1:v]scale=80:-1[logo]; \
-     [2:v]scale=200:-1[ads]; \
-     [0:v][logo]overlay=main_w-overlay_w-20:20[v1]; \
-     [v1][ads]overlay=main_w-overlay_w-20:main_h-overlay_h-20[v2]; \
-     [v2]drawtext=fontsize=32:fontcolor=white:box=1:boxcolor=black@0.5:boxborderw=8:x=20:y=20:text='%{localtime\:%H\:%M}'"[v] \
+    "[1:v]scale=iw*0.35:-1[logo]; \
+     [0:v][logo]overlay=main_w-overlay_w-20:20[v]" \
     -map "[v]" -map 0:a \
     -c:v libx264 -preset ultrafast -b:v 1500k -maxrate 1500k -bufsize 3000k \
     -pix_fmt yuv420p -g 50 -c:a aac -b:a 96k -ar 44100 \
